6cec765eee84c173a8fdd269c705abf6e34e2abd
AHMAD_KHATIB <ahmadkhatib923@gmail.com>
2022-04-30 00:26:46 +0300

    initialize pusher and laravel-echo

--- real-time-first-project/resources/views/layouts/app.blade.php
+++ real-time-first-project/resources/views/layouts/app.blade.php
@@ -9,9 +9,6 @@
 
     <title>{{ config("app.name", "Laravel") }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com" />
     <link
@@ -21,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    @stack("styles")
   </head>
   <body>
     <div id="app">
@@ -106,5 +104,8 @@
 
       <main class="py-4">@yield('content')</main>
     </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    @stack("scripts")
   </body>
 </html>
